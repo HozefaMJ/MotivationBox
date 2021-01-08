2d4ca3cbd219bdfd0a3193d29b86239eaa567913
Hozefa <hozefa24imp@gmail.com>
2021-01-08 22:31:33 +0530

    BE - authMiddleware | seeder.js | tested DB Insertion

---
+++ backend/data/posts.js
@@ -0,0 +1,14 @@
+const posts = [
+    {
+      title: '2021 Goals',
+      post: 'Earn 10 lacs' 
+    },
+    {
+        title: '2021 Goals',
+        post: 'Earn 10 lacs' 
+    },
+    
+  ]
+  
+  export default posts
+  
